b29e810a7e1521973958cfc3a437037d8376bdb3
Chris Smith <chris@zxdesign.info>
2016-02-15 13:42:48 +0000

    Added authorisation documentation.

--- spec/spec.go
+++ spec/spec.go
@@ -9,7 +9,7 @@ import (
 
 	//"github.com/companieshouse/swaggerly/logger"
 	"github.com/companieshouse/go-swagger/spec"
-	"github.com/davecgh/go-spew/spew"
+	//"github.com/davecgh/go-spew/spew"
 	"github.com/serenize/snaker"
 	"github.com/shurcooL/github_flavored_markdown"
 )
