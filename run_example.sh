862e816084ab3589bc3097a8c3e570c08b1d3db5
Chris Smith <chris@zxdesign.info>
2016-09-12 21:13:11 +0100

    Reverence petstore spec within examples/*

--- run_example.sh
+++ run_example.sh
@@ -1,5 +1,5 @@
 ./swaggerly \
-    -spec-dir=petstore \
+    -spec-dir=examples/specifications/petstore/ \
     -bind-addr=0.0.0.0:3123 \
     -spec-rewrite-url=api.uber.com=API.UBER.COM \
     -document-rewrite-url=www.google.com=www.google.co.uk \
