cd04c2086f8058d15b035008a316db011f7082f1
Chris Smith <chris@zxdesign.info>
2016-09-22 18:14:52 +0100

    Added info log to guide registration.

--- handlers/guides/guides.go
+++ handlers/guides/guides.go
@@ -35,7 +35,7 @@ func Register(r *pat.Router) {
 
 	base := GetBasePath()
 
-	logger.Debugf(nil, "Registering guides")
+	logger.Infof(nil, "Registering guides")
 
 	// specification specific guides
 	for _, specification := range spec.APISuite {
