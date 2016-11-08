1c8158a894dadfdb889e3e34c83f4758be92f436
Chris Smith <chris@zxdesign.info>
2016-11-08 20:42:05 +0000

    New logos and trimmed down start page.

--- js/script.js
+++ js/script.js
@@ -34,23 +34,6 @@ $(document).ready(function() {
 		//MagnificPopup
 		$('.image-link').magnificPopup({type:'image'});
 
-
-		// OwlCarousel N1
-		$("#owl-demo").owlCarousel({
-			autoPlay: 3000,
-			items : 3,
-			itemsDesktop : [1199,3],
-			itemsDesktopSmall : [979,3]
-		});
-
-		// OwlCarousel N2
-		$("#owl-demo-1").owlCarousel({
-			  navigation : false, // Show next and prev buttons
-			  slideSpeed : 300,
-			  paginationSpeed : 400,
-			  singleItem:true
-		});
-
 		//SmothScroll
 		$('a[href*=#]').click(function() {
 			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
@@ -65,12 +48,5 @@ $(document).ready(function() {
 			}
 		});
 		
-		//Subscribe
-		/* new UIMorphingButton( document.querySelector( '.morph-button' ) ); */
-		// for demo purposes only
-		[].slice.call( document.querySelectorAll( 'form button' ) ).forEach( function( bttn ) { 
-			bttn.addEventListener( 'click', function( ev ) { ev.preventDefault(); } );
-		} );
-
 });
 
