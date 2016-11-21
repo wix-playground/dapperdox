a0eebb19f31243916d9f84ced95b582b2ef2a788
Chris Smith <chris@zxdesign.info>
2016-11-21 15:40:49 +0000

    Fixed download file extension, and restored CNAME

--- src/build.sh
+++ src/build.sh
@@ -3,16 +3,22 @@ wget --recursive --no-host-directories http://localhost:3100/index.html -o src/w
 
 # Now deal with favicons that are searched for by browsers
 cp -r src/images/fav/* images/fav/.
-cp src/browserconfig.xml .
+cp    src/browserconfig.xml .
 
 cd docs
-for f in `ls`
+for f in `ls | egrep -v "\.html$"`
 do
     mv ${f} ${f}.html
 done
+cd -
+
+mv downloads/download downloads/download.html
+echo "dapperdox.io" > CNAME
 
 for f in `find . -type f | grep -v "^\./src" | grep -v "^\./\.git"`
 do
     git add ${f}
 done
+git add CNAME
+git add downloads/*
 
