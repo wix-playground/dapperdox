c7325e1b49b03782fb80cbe526bb299afbff2113
Chris Smith <chris@zxdesign.info>
2016-11-21 13:27:23 +0000

    Static build of site.

--- src/build.sh
+++ src/build.sh
@@ -4,3 +4,8 @@ wget --recursive --no-host-directories http://localhost:3100/xindex.html -o src/
 # Now deal with favicons that are searched for by browsers
 cp -r src/images/fav/* images/fav/.
 cp src/browserconfig.xml .
+
+for f in `find . -type f | grep -v "^\./src" | grep -v "^\./\.git"`
+do
+    git add ${f}
+done
