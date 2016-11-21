4e6ee0c13382ae0b45f218f86c0f55c578e172c4
Chris Smith <chris@zxdesign.info>
2016-11-21 14:43:45 +0000

    Fix files by adding html ext.

--- src/build.sh
+++ src/build.sh
@@ -1,11 +1,18 @@
 # Crawl site
-wget --recursive --no-host-directories http://localhost:3100/xindex.html -o src/wget.log
+wget --recursive --no-host-directories http://localhost:3100/index.html -o src/wget.log
 
 # Now deal with favicons that are searched for by browsers
 cp -r src/images/fav/* images/fav/.
 cp src/browserconfig.xml .
 
+cd docs
+for f in `ls`
+do
+    mv ${f} ${f}.html
+done
+
 for f in `find . -type f | grep -v "^\./src" | grep -v "^\./\.git"`
 do
     git add ${f}
 done
+
