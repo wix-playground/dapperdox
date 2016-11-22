04f496da6cbb4e69b2f5c9f0405228a98376207d
Chris Smith <chris@zxdesign.info>
2016-11-22 17:13:40 +0000

    Fixes issue #21 - specification download URL now relative to site.

--- run_example.sh
+++ run_example.sh
@@ -4,10 +4,10 @@
     -site-url=http://localhost:3123 \
     -spec-rewrite-url=petstore.swagger.io=PETSTORE.swagger.io \
     -theme=default  \
-    -log-level=info
+    -log-level=trace \
+    #-tls-certificate=server.rsa.crt \
+    #-tls-key=server.rsa.key \
     #-author-show-assets=true \
     #-force-specification-list=true \
     #-assets-dir=./examples/overlay/assets \
-    #-tls-certificate=server.rsa.crt \
-    #-tls-key=server.rsa.key \
     #-proxy-path=/developer=https://developer.some-dev-site.com 
