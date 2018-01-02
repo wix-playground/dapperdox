f6114b9522fdea0c5e55eb50cf7e503d3616232e
sharonk <sharonk@wix.com>
2018-01-02 11:47:51 +0200

    #AUTODEV-4514 logging

--- spec/spec.go
+++ spec/spec.go
@@ -438,6 +438,12 @@ func getMainSchema(api *APIGroup, tagName string) string {
 				logger.Infof(nil, tagName + ": with property title: " + property.Title)
 				if strings.Replace(property.Title, " ", "", -1) == strings.Replace(tagName, " ", "", -1)  {
 					logger.Infof(nil, "YESSSSSSSSSSSSSS: " + tagName + " SUCCEDDED with property schema of: " + property.Schema)
+					rout, err := json.Marshal(r)
+					if err != nil {
+						panic (err)
+					}
+					logger.Infof(nil, "ROUT IS: " + string(rout))
+
 					out, err := json.Marshal(property)
 					if err != nil {
 						panic (err)
